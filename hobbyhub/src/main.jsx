44dd244e2d461a3413557cf15cfc9219fa50af4f
Mateo Gomez <134739014+FineAgedCheddar@users.noreply.github.com>
2024-04-28 10:08:19 -0400

    Update main.jsx

--- hobbyhub/src/main.jsx
+++ hobbyhub/src/main.jsx
@@ -15,9 +15,8 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <SearchProvider>
       <BrowserRouter>
         <Routes>
-          <Route path ="/" element={<Taskbar />}>
-            <Route index = {true} path = "/" element={<ViewAll />} />
-          </Route>
+          <Route path ="/" element={<Taskbar />}/>
+          <Route index = {true} path = "/" element={<ViewAll />} />
           <Route index = {false} path = "/createPost" element = {<CreatePost />} />
           <Route index = {false} path = "/postDetails/:id" element = {<PostDetails />} />
           <Route index = {false} path = "/editPost/:id" element = {<EditPost />} />
