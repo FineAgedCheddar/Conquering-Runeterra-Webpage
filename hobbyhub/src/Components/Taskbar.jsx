857f74c2aaa41944d74999c15f5590dc5f9a0d1d
Mateo Gomez <134739014+FineAgedCheddar@users.noreply.github.com>
2024-04-28 08:50:30 -0400

    Update Taskbar.jsx

--- hobbyhub/src/Components/Taskbar.jsx
+++ hobbyhub/src/Components/Taskbar.jsx
@@ -2,15 +2,31 @@ import { Outlet, Link } from "react-router-dom";
 import { useState } from "react";
 import { useContext } from 'react';
 import { SearchContext } from '../context/SearchContext';
-
-
+import { supabase } from "../Client.js;
 import "./Taskbar.css";
+
 const Taskbar = () => {
     const { setUserSearch } = useContext(SearchContext);
 
     const handleChange = (event) => {
         setUserSearch(event.target.value);
     };
+
+    const [post, setPost] = useState(null);
+    
+    useEffect( ()=> {
+        const fetchPosts = async () => {
+                let { data } = await supabase.from("post").select().order("created_at", {ascending: false});
+                setPost(data);
+                
+            } 
+
+    
+        fetchPosts()
+        
+    },[]);
+
+    
     return( 
         
         <>
@@ -20,6 +36,7 @@ const Taskbar = () => {
                 <button>⌕</button>
                 <Link className = "home-b" style={{ color: 'white' }} to = "/"><h2>Home</h2></Link>
                 <Link className = "create-b" style={{ color: 'white' }} to = "/createPost"><h2>Create Post</h2></Link>
+                {post? console.log(post) : null}
             </div>
             <Outlet /> 
         </>
